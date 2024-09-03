4543077780d62076db7c7f299dbc9e4c212b39cc
IGAL KOPELMAN <igalkop@gmail.com>
2024-09-03 16:10:36 +0300

    Add food repository (#7)
    * update readme file
    
    * add Food Repository

--- store/src/main/java/com/ikop/diet/service/TestService.java
+++ store/src/main/java/com/ikop/diet/service/TestService.java
@@ -1,13 +1,26 @@
 package com.ikop.diet.service;
 
 import com.ikop.diet.model.Food;
+import com.ikop.diet.repository.FoodRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TestService {
 
-    public TestService() {
-        Food food = new Food("milk", 3);
-        System.out.println("food = " + food);
-    }
+    private final FoodRepository foodRepository;
+
+//    public TestService() {
+//        Food food = new Food("milk", 3);
+//        System.out.println("food = " + food);
+//    }
+
+//    @PostConstruct
+//    public void init(){
+//        foodRepository.save(new Food("milk", 10));
+//        System.out.println("listing all foods so far...");
+//        foodRepository.findAll().forEach(System.out::println);
+//    }
 }
