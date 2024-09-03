cae53182a01bb993587f56fa004374e3b2d94ac7
Igal <igalkop@gmail.com>
2024-09-03 22:07:24 +0300

    add Food controller

--- store/src/main/java/com/ikop/diet/repository/FoodRepository.java
+++ store/src/main/java/com/ikop/diet/repository/FoodRepository.java
@@ -3,5 +3,5 @@ package com.ikop.diet.repository;
 import com.ikop.diet.model.Food;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface FoodRepository extends MongoRepository<Food, Integer> {
+public interface FoodRepository extends MongoRepository<Food, String> {
 }
