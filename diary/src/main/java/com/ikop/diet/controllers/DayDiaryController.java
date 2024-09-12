56b9f6bb6e612ef4d43eb7a0f2112554dff394f9
Igal <igalkop@gmail.com>
2024-09-12 15:47:41 +0300

    code cleanup and dependencies refactor

--- diary/src/main/java/com/ikop/diet/controllers/DayDiaryController.java
+++ diary/src/main/java/com/ikop/diet/controllers/DayDiaryController.java
@@ -1,22 +1,10 @@
 package com.ikop.diet.controllers;
 
-import com.ikop.diet.model.Day;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/status")
+@RequestMapping("/diary")
 public class DayDiaryController {
-
-    @GetMapping
-    public ResponseEntity<Day> getCurrentDayStatus() {
-        Day day = new Day();
-        day.setId("111");
-        day.setName("food1");
-        day.setRemainingPoints(1.2);
-        return ResponseEntity.ok(day);
-    }
 
 }
