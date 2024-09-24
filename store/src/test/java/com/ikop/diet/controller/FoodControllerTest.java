319c98f0973af51bf86b0862b04333fdc0a18ea8
Igal <igalkop@gmail.com>
2024-09-24 12:35:55 +0300

    refactor UTs

--- store/src/test/java/com/ikop/diet/controller/FoodControllerTest.java
+++ store/src/test/java/com/ikop/diet/controller/FoodControllerTest.java
@@ -7,9 +7,13 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCollection;
@@ -18,66 +22,37 @@ class FoodControllerTest {
 
     private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
-
-    @Test
-    void validateEmptyFoodDtoBean() {
-        List<String> expectedMessages = List.of(
-                "id cannot be empty",
-                "name cannot be empty",
-                "points cannot be null",
-                "description cannot be empty"
+    public static Stream<Arguments> dtoTestProvider() {
+        return Stream.of(
+                Arguments.of(new FoodDTO(), List.of(
+                        "id cannot be empty",
+                        "name cannot be empty",
+                        "points cannot be null",
+                        "description cannot be empty"
+                )),
+                Arguments.of(new FoodDTO("123", "name", 0d, "description"), List.of("minimal value of points is 0.1")),
+                Arguments.of(new FoodUpdateDTO(), List.of(
+                        "id cannot be empty",
+                        "name cannot be empty",
+                        "points cannot be null",
+                        "description cannot be empty"
+                )),
+                Arguments.of(new FoodUpdateDTO("123", "name", 0d, "description"), List.of("minimal value of points is 0.1"))
         );
-        FoodDTO foodDTO = new FoodDTO();
+    }
 
-        Set<ConstraintViolation<FoodDTO>> errors = validator.validate(foodDTO);
+
+    @ParameterizedTest
+    @MethodSource("dtoTestProvider")
+    void testDtosForValidation(FoodDTO inputFoodDto, List<String> expectedErrors) {
+        Set<ConstraintViolation<FoodDTO>> errors = validator.validate(inputFoodDto);
 
         assertThat(errors).isNotEmpty();
         List<String> actualErrorList = errors.stream().map(ConstraintViolation::getMessage).toList();
-        assertThatCollection(actualErrorList).containsExactlyInAnyOrderElementsOf(expectedMessages);
+        assertThatCollection(actualErrorList).containsExactlyInAnyOrderElementsOf(expectedErrors);
     }
 
-    @Test
-    void assertForNegativePointsFoodDto() {
-        List<String> expectedMessages = List.of("minimal value of points is 0.1");
-        FoodDTO foodDTO = new FoodDTO("123", "name", 0d, "description");
 
-
-        Set<ConstraintViolation<FoodDTO>> errors = validator.validate(foodDTO);
-
-        assertThat(errors).isNotEmpty();
-        List<String> actualErrorList = errors.stream().map(ConstraintViolation::getMessage).toList();
-        assertThatCollection(actualErrorList).containsExactlyInAnyOrderElementsOf(expectedMessages);
-    }
-
-    @Test
-    void validateEmptyFoodUpdateDtoBean() {
-        List<String> expectedMessages = List.of(
-                "id cannot be empty",
-                "name cannot be empty",
-                "points cannot be null",
-                "description cannot be empty"
-        );
-        FoodUpdateDTO foodUpdateDTO = new FoodUpdateDTO();
-
-        Set<ConstraintViolation<FoodUpdateDTO>> errors = validator.validate(foodUpdateDTO);
-
-        assertThat(errors).isNotEmpty();
-        List<String> actualErrorList = errors.stream().map(ConstraintViolation::getMessage).toList();
-        assertThatCollection(actualErrorList).containsExactlyInAnyOrderElementsOf(expectedMessages);
-    }
-
-    @Test
-    void assertForNegativePointsFoodUpdateDto() {
-        List<String> expectedMessages = List.of("minimal value of points is 0.1");
-        FoodUpdateDTO foodUpdateDTO = new FoodUpdateDTO("123", "name", 0d, "description");
-
-
-        Set<ConstraintViolation<FoodUpdateDTO>> errors = validator.validate(foodUpdateDTO);
-
-        assertThat(errors).isNotEmpty();
-        List<String> actualErrorList = errors.stream().map(ConstraintViolation::getMessage).toList();
-        assertThatCollection(actualErrorList).containsExactlyInAnyOrderElementsOf(expectedMessages);
-    }
 
     @Test
     void validateEmptyFoodCreateDtoBean() {
